ff0c99afa502a7350de25cebff3249161cc7c346
Chris Bell <cjbell1988@gmail.com>
2021-11-11 13:29:09 -0700

    fix: notification cell a11y fixes, add new onMarkAllAsReadClick prop

--- src/components/NotificationFeedPopover/NotificationFeedPopover.tsx
+++ src/components/NotificationFeedPopover/NotificationFeedPopover.tsx
@@ -13,47 +13,51 @@ export interface NotificationFeedPopoverProps extends NotificationFeedProps {
   closeOnClickOutside?: boolean;
 }
 
-export const NotificationFeedPopover: React.FC<NotificationFeedPopoverProps> = ({
-  isVisible,
-  onClose,
-  buttonRef,
-  closeOnClickOutside = true,
-  ...feedProps
-}) => {
-  const { colorMode } = useKnockFeed();
-  const { ref: popperRef } = useComponentVisible(isVisible, onClose, {
-    closeOnClickOutside,
-  });
+export const NotificationFeedPopover: React.FC<NotificationFeedPopoverProps> =
+  ({
+    isVisible,
+    onClose,
+    buttonRef,
+    closeOnClickOutside = true,
+    ...feedProps
+  }) => {
+    const { colorMode } = useKnockFeed();
+    const { ref: popperRef } = useComponentVisible(isVisible, onClose, {
+      closeOnClickOutside,
+    });
 
-  const { styles, attributes } = usePopper(
-    buttonRef.current,
-    popperRef.current,
-    {
-      strategy: "fixed",
-      placement: "bottom-end",
-      modifiers: [
-        {
-          name: "offset",
-          options: {
-            offset: [0, 8],
+    const { styles, attributes } = usePopper(
+      buttonRef.current,
+      popperRef.current,
+      {
+        strategy: "fixed",
+        placement: "bottom-end",
+        modifiers: [
+          {
+            name: "offset",
+            options: {
+              offset: [0, 8],
+            },
           },
-        },
-      ],
-    }
-  );
+        ],
+      }
+    );
 
-  return (
-    <div
-      className={`rnf-notification-feed-popover rnf-notification-feed-popover--${colorMode}`}
-      style={{ ...styles.popper, visibility: isVisible ? "visible" : "hidden" }}
-      ref={popperRef}
-      {...attributes.popper}
-      role="dialog"
-      tabIndex={-1}
-    >
-      <div className="rnf-notification-feed-popover__inner">
-        <NotificationFeed isVisible={isVisible} {...feedProps} />
+    return (
+      <div
+        className={`rnf-notification-feed-popover rnf-notification-feed-popover--${colorMode}`}
+        style={{
+          ...styles.popper,
+          visibility: isVisible ? "visible" : "hidden",
+        }}
+        ref={popperRef}
+        {...attributes.popper}
+        role="dialog"
+        tabIndex={-1}
+      >
+        <div className="rnf-notification-feed-popover__inner">
+          <NotificationFeed isVisible={isVisible} {...feedProps} />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
