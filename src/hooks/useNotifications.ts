0e0aff8800a81b5b161667559ecbe7099c06503a
Chris Bell <cjbell1988@gmail.com>
2022-07-14 17:46:05 -0500

    add missing hook

---
+++ src/hooks/useNotifications.ts
@@ -0,0 +1,23 @@
+import Knock, { Feed, FeedClientOptions } from "@knocklabs/client";
+import { useMemo, useRef } from "react";
+
+function useNotifications(
+  knock: Knock,
+  feedId: string,
+  options: FeedClientOptions = {}
+) {
+  const feedClientRef = useRef<Feed | null>();
+
+  return useMemo(() => {
+    if (feedClientRef.current) feedClientRef.current.teardown();
+
+    const feedClient = knock.feeds.initialize(feedId, options);
+
+    feedClient.listenForUpdates();
+    feedClientRef.current = feedClient;
+
+    return feedClient;
+  }, [knock, feedId, options.source, options.tenant]);
+}
+
+export default useNotifications;
