d49d6277bfe65464e4eb16d65c6a2062ab9ba9d7
Alexander Kavanaugh <alex@kavdev.io>
2023-06-07 06:54:26 -0700

    Export useOnBottomScroll hook (#31)

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,3 @@
 export { default as useAuthenticatedKnockClient } from "./useAuthenticatedKnockClient";
+export { default as useOnBottomScroll } from "./useOnBottomScroll";
 export { default as useNotifications } from "./useNotifications";
